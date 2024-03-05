06d05961c90988281d3e374001dfcbc0abf4beb0
Iqra Fatima <iqraf683@gmail.com>
2024-03-05 15:46:57 +0500

    Code-updated

---
+++ components/Navbar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { TfiEmail } from "react-icons/tfi";
+import { GrProjects } from "react-icons/gr";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between bottom-0 items-center gap-4  bg-transparent w-screen h-[8vh] sm:w-[40px] sm:h-[40vh] sm:flex-col absolute sm:top-[30%]  sm:right-0 p-4">
+      <NavLink
+        to="/Section1"
+        className={(e) => {
+          return e.isActive ? "text-black" : "text-gray-500";
+        }}
+      >
+        <FaHome />
+      </NavLink>
+      <NavLink
+        to="/Section2"
+        className={(e) => {
+          return e.isActive ? "text-black" : "text-gray-500";
+        }}
+      >
+        <FaUser />
+      </NavLink>
+      <NavLink
+        to="/Section3"
+        className={(e) => {
+          return e.isActive ? "text-gray-950" : "text-gray-500";
+        }}
+      >
+        <GrProjects />
+      </NavLink>
+      <NavLink
+        to="/Section4"
+        className={(e) => {
+          return e.isActive ? "text-gray-950" : "text-gray-500";
+        }}
+      >
+        <TfiEmail />
+      </NavLink>
+    </div>
+  );
+};
+
+export default Navbar;
